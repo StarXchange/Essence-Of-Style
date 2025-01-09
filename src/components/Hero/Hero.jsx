ca924e7db1ced09ad49b5bf4b3da6e88d1bf9148
PunkHazard07 <belloayoola20@gmail.com>
2025-01-09 15:39:35 +0100

    hero section, Navbar

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import image1 from "../../assets/image1.png";
 
 const Hero = () => {
   return (
-    <div className='flex flex-col sm:flex-row border border-gray-400'>
+    <div className='flex flex-col sm:flex-row border h-[400px] border-gray-400'>
         {/* Hero Left Side */}
         <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>
             <div className='text-[#414141]'>
@@ -18,7 +19,7 @@ const Hero = () => {
             </div>
         </div>
         {/*Hero Right Side  */}
-        
+        <img className='w-full sm:w-1/2' justify-content-between src={image1} alt="" />
     </div>
   )
 }
