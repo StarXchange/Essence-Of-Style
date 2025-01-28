b06c9d7cc0275ebb2b62cbf74ae958dc2cb7fe51
StarXchange <sammygeorge385@gmail.com>
2025-01-28 15:34:23 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
       
       <Routes>
 
-        <Route path="/" element={<Home />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Login />} />
